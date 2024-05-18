ad0aad9baea84a6f2a7b4a72e02cc73de5cfa8b5
Tlotliso Mpeoa <129188626+BonoloMilli@users.noreply.github.com>
2024-05-18 23:20:42 +0200

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,13 +1,1 @@
-class TakeDetails:
-    def __init__(self, name, surname, idNum):
-        self.name = name
-        self.surname = surname
-        self.idNum = idNum
-
-    def register(self):
-        clientName = self.name
-        clientSurname = self.surname
-        return clientName, clientSurname
-    def reg(self):
-       clientName = self.name
 
