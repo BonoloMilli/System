261227d15d28261e46f229145c1895f860112e0b
Bri7822 <brightchester5@gmail.com>
2024-05-19 01:13:47 +0200

    Last commit

--- Practice.py
+++ Practice.py
@@ -1,31 +1,30 @@
-<<<<<<< HEAD
-class TakeDetails:
-    def __init__(self, name, surname, idNum):
-        self.name = name
-        self.surname = surname
-        self.idNum = idNum
-        
-    def register(self):
-       for studNo in list:
-         if studNo === promptedStudNo:
 
-       def reg(self):
-       clientName = self.name
-
-=======
 class Clients(object):
-    def __init__(self, studentNum):
+   
+   
+    def __init__(self, studentNum,mark1,mark2):
         self.studentNum = studentNum
-
+        self.mark1 = mark1
+        self.mark2 = mark2
     def compare(self):
-        students = ["214", "265", "256", "245", "888", "254"]
+        students = [("214"), ("265"), ("256"), ("245"), ("888"), ("254")]
+        stuMarks = []
         if self.studentNum not in students:
             print("You are not in the system")
+        else:
+              print("WELCOME BACK")
+              
+        for i,student in enumerate(students):
+         if  student == self.studentNum:
+             stuMarks[i] = self.mark1
+             stuMarks[i] = self.mark2
+             print(f"{self.studentNum} {self.mark1} {self.mark2}") 
+         else:
+                ("Invalid marks/s")
+                                
 
-# Example usage
-client = Clients("123")
-client.compare()  # Output: You are not in the system
-
-client = Clients("214")
-client.compare()  # No output, since 214 is in the list
->>>>>>> c8cba151c96670cf059032bc748fd636cc5a897d
+stud = int(input("Enter Student Number:"))
+test1 = int(input("Test 1:"))
+test2 = int(input("Test 2:"))
+obj = Clients(stud,test1,test2)
+obj.compare()
