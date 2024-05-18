7f7d5f2882f048f688a18c02b0e20ea28af5fd44
Tlotliso Mpeoa <Tlotliso2062@gmail.com>
2024-05-18 23:26:27 +0200

    Tlotliso Commit

--- Practice.py
+++ Practice.py
@@ -1,7 +1,15 @@
-class clients (object):
-    def __init__(self,studentNum):
+class Clients(object):
+    def __init__(self, studentNum):
         self.studentNum = studentNum
+
     def compare(self):
-        student = ["214","265","256","245","888","254"]
-        if self.studentNum != student[0:]:
-            print("Get the fuck out of our system")
+        students = ["214", "265", "256", "245", "888", "254"]
+        if self.studentNum not in students:
+            print("You are not in the system")
+
+# Example usage
+client = Clients("123")
+client.compare()  
+
+client = Clients("214")
+client.compare()  
