807ef81e435a893de2dccf54250ff71af3502858
Bri7822 <brightchester5@gmail.com>
2024-05-21 00:05:21 +0200

    Code can execute Now

--- Practice.py
+++ Practice.py
@@ -6,19 +6,23 @@ class Clients(object):
         self.studentNum = studentNum
         self.mark1 = mark1
         self.mark2 = mark2
+        self.students = [{'studNo':214, 'mak1': None, 'mak2':None}, 
+                         {'studNo':233, 'mak1': None, 'mak2':None},
+                         {'studNo':776, 'mak1': None, 'mak2':None},
+                         {'studNo':543, 'mak1': None, 'mak2':None},
+                         {'studNo':278, 'mak1': None, 'mak2':None}]
+        
     def compare(self):
-        students = [("214"), ("265"), ("256"), ("245"), ("888"), ("254")]
-        stuMarks = []
-        if self.studentNum not in students:
-            print("You are not in the system")
+        if not any(student['studNo'] == self.studentNum for student in self.students):
+            print("You are not in the system")    
         else:
               print("WELCOME BACK")
-              
-        for i,student in enumerate(students):
-         if  student == self.studentNum:
-             stuMarks[i] = self.mark1
-             stuMarks[i] = self.mark2
-             print(f"{self.studentNum} {self.mark1} {self.mark2}") 
+    def inputsMarks(self):
+        for student in self.students:
+         if self.studentNum == student['studNo']:
+               student['mak1'] = self.mark1
+               student['mak2'] = self.mark2
+               print(f"{self.studentNum} {student['mak1']} {student['mak2']}") 
          else:
                 ("Invalid marks/s")
                                 
@@ -28,3 +32,4 @@ test1 = int(input("Test 1:"))
 test2 = int(input("Test 2:"))
 obj = Clients(stud,test1,test2)
 obj.compare()
+obj.inputsMarks()
